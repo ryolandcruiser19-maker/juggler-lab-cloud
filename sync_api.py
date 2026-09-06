93e9c4c11faf499a70da4c0bc1049e7fb082fb0a
ryolandcruiser19-maker <ryo.landcruiser.19@gmail.com>
2026-09-06 11:21:55 +0900

    Update sync_api.py

--- sync_api.py
+++ sync_api.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/sync-test")
-def sync_test():
-    return {"status": "ok"}
+class SyncTestData(BaseModel):
+    date: str
+    machine_no: int
+    machine_name: str
+
+
+@app.post("/sync-test")
+def sync_test(data: SyncTestData):
+    return {
+        "status": "ok",
+        "received": {
+            "date": data.date,
+            "machine_no": data.machine_no,
+            "machine_name": data.machine_name,
+        },
+    }
